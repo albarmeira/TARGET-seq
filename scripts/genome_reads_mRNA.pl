f6cba429388affe3746de336224ce978d3852575
albarmeira <32486992+albarmeira@users.noreply.github.com>
2018-11-05 23:58:07 +0000

    Add files via upload

---
+++ scripts/genome_reads_mRNA.pl
@@ -0,0 +1,176 @@
+#!/usr/local/bin/perl -w
+
+use strict;
+use Data::Dumper;
+use Getopt::Long;
+use Config::Simple;
+
+my $cfg = new Config::Simple('SCpipe.conf');
+my %config = $cfg->vars();
+
+$| = 1;
+
+&GetOptions
+(
+	"c=s"=>\my $coords_file,
+	"b=s"=> \my $bam_dir,
+	"o=s"=> \my $out_dir
+);
+unless($coords_file) {die "ERROR: no coords file!\nUSAGE: genome_reads.pl -c full/path/coords.bed -b /full/path/bam/ -o full/path/output/\n\n";}
+unless($bam_dir) {die "ERROR: no bam files dir!\ngenome_reads.pl -c full/path/coords.bed -b /full/path/bam/ -o full/path/output/\n\n";}
+unless($out_dir) {die "ERROR: no output dir!\ngenome_reads.pl -c full/path/coords.bed -b /full/path/bam/ -o full/path/output/\n\n";}
+unless($bam_dir =~ m/\/$/) {$bam_dir .= '/';}
+unless($out_dir =~ m/\/$/) {$out_dir .= '/';}
+
+my %coords;
+open (COORDS, "<$coords_file") or die "cannot open $coords_file: $!\n";
+while (<COORDS>)
+{
+	my $line = $_;
+	chomp $line;
+	unless ($line){next;}
+	if ($line =~ m/^Chr/) {next;}
+	
+	my ($chr, $from, $to, $amplicon) = split(/\t/, $line);
+	unless($chr =~ m/^chr/) {$chr = 'chr' . $chr;}
+	
+	$coords{$chr}{$from}{to} = $to;
+	$coords{$chr}{$from}{amplicon} = $amplicon;
+}
+close COORDS;
+
+
+
+opendir(DIR, $bam_dir);
+my @files = readdir(DIR);
+closedir(DIR);
+ 
+foreach my $file (@files)
+{
+	if ($file eq "." or $file eq "..") {next;}
+	unless ($file =~ m/bam$/) {next;}
+	
+	my $genome_sam_file = $file;
+	$genome_sam_file =~ s/bam$/genome.sam/;
+	
+	$file = $bam_dir . $file;
+	$genome_sam_file = $out_dir . $genome_sam_file;
+	
+	my $cmd = $config{SAMTOOLS_EXECUTABLE} . ' view ' . $file;
+	my %sam_data = ();
+	my %genome_read_ids = ();
+	open (PIPE, "$cmd |") || die "$!";
+	while (<PIPE>)
+	{
+		my $line = $_;  
+		chomp $line;
+		
+		my ($read_id, $bitwise_flag, $ref_seq, $start, $qual, $cigar, $read_pair_ref_seq, $read_pair_start, $insert_size, $seq, $bp_scores, $optional_field) = split(/\t/, $line);
+		$sam_data{$read_id}{$ref_seq}{$start} = $line;
+		
+		my ($bits_ref) = &parseSAMbitwise($bitwise_flag);
+		my %bits = %{$bits_ref};
+		
+		# are there any skipped bases (ie spliced reads arising from mRNA)?
+		my $skipped = 0;
+		while ($cigar =~ m/(\d+)N/g)
+		{
+			$skipped += $1;
+		}
+		#print "Skipped: $skipped\n-------------\n";
+		
+		# check read is mapped in a proper pair:
+		unless(exists($bits{b2})) {next;}
+		
+		foreach my $from_coord (keys %{$coords{$ref_seq}})
+		{
+			my $to_coord = $coords{$ref_seq}{$from_coord}{to};
+			my $mid_posn = (($to_coord - $from_coord) / 2);
+			my $amplicon = $coords{$ref_seq}{$from_coord}{amplicon};
+
+			if(exists($bits{b10}))
+			{
+				# reads on reverse strand:
+				my $adjusted_start = ($start - $mid_posn) + length($seq) + $skipped;
+				if (($adjusted_start >= $from_coord) and ($adjusted_start <= $to_coord))
+				{
+					$genome_read_ids{$read_id}{$ref_seq}{$start} = '';
+				}
+			}
+			else
+			{
+				# reads on fwd strand:
+				my $adjusted_start = ($start + $mid_posn);
+				#my $adjusted_start = ($start);
+				if (($adjusted_start >= $from_coord) and ($adjusted_start <= $to_coord))
+				{
+					$genome_read_ids{$read_id}{$ref_seq}{$start} = '';
+				}
+			}
+		}
+	}
+	close PIPE;	
+
+	#die Dumper (\%sam_data);
+
+	my $count = 0;
+	open (OUT, ">$genome_sam_file") or die "cannot open out file: $genome_sam_file: $!\n";
+	foreach my $read_id (sort keys %genome_read_ids)
+	{
+		foreach my $read_chr (keys %{$genome_read_ids{$read_id}})
+		{
+			foreach my $read_coord (sort numerically keys %{$genome_read_ids{$read_id}{$read_chr}})
+			{
+				print OUT $sam_data{$read_id}{$read_chr}{$read_coord} . "\n";
+				$count ++;
+			}
+		}
+	}
+	close OUT;
+	
+	print "Reads: $count\n";
+
+}
+
+
+#----------------------------------------------------------------------------------------
+
+sub parseSAMbitwise
+{
+############################################
+# Interpret bitwise flag form the SAM format (http://samtools.sourceforge.net/SAM1.pdf)
+# Input:  flag
+# Output: reference to a hash of set bits and associated values
+############################################
+
+	my ($flag) = @_;
+	
+	my %bits;
+	
+	if($flag == 0)
+	{
+		$bits{'b0'} = 'mapped to forward strand, not paired-end';
+		return(\%bits);
+	}
+	
+	if ($flag & 0x0001) {$bits{b1} = 'the read is paired in sequencing, (but not necesarily mapped in a pair)';}
+	if ($flag & 0x0002) {$bits{b2} = 'the read is mapped in a proper pair (depends on the protocol, normally inferred during alignment)';}
+	if ($flag & 0x0004) {$bits{b4} = 'the query sequence itself is unmapped';}
+	if ($flag & 0x0008) {$bits{b8} = 'the pair mate is unmapped';}
+	if ($flag & 0x0010) {$bits{b10} = 'mapped to reverse strand';}
+	if ($flag & 0x0020) {$bits{b20} = 'pair mate is mapped to reverse strand';}
+	if ($flag & 0x0040) {$bits{b40} = 'the read is the first read in a pair';}
+	if ($flag & 0x0080) {$bits{b80} = 'the read is the second read in a pair';}
+	if ($flag & 0x0100) {$bits{b100} = 'the alignment is not primary (a read having split hits may have multiple primary alignment records)';}
+	if ($flag & 0x0200) {$bits{b200} = 'the read fails platform/vendor quality checks';}
+	if ($flag & 0x0400) {$bits{b400} = 'the read is either a PCR duplicate or an optical duplicate';}
+	
+	return(\%bits);	
+}
+
+
+
+sub numerically
+{
+	$a <=> $b;
+}
---
+++ scripts/getcounts_mpileup_TARGET.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+inFile = open(sys.argv[1],'r')
+FileName = os.path.basename(sys.argv[1])
+FileName1 = FileName.replace('.mpileup','')
+
+if os.path.getsize(sys.argv[1])>0:
+	
+	for line in inFile:
+
+		############################
+		#Initiate types for output
+		types = {'A':0,'G':0,'C':0,'T':0,'-':0,'+':[],'X':[]}
+
+		############################
+		# Read file if not empty
+		if len(line) > 0:
+
+			############################
+			#Check healthy data 
+			data = line.strip().split('\t')
+			doctor = data[0]
+			if "chr" in doctor and (len(data)>=5):  #len(data)>5 because some .mpileup files are corrupted and only contain 4 fields
+	
+				############################
+				#Obtain all info from line
+				chromosome= data[0]
+				bp = data[1]
+				bases = data[4].upper()
+
+				############################
+				#Parse information
+				i = 0
+				jump = 0
+				while i < len(bases):
+					base = bases[i]
+					if base == '^' or base == '$':
+						i += 1
+					elif base == '-':
+						i += 1
+						types['-'] += 1
+						if bases[i+1].isdigit():    # Addition for deletions longer than 9 bp
+							jump = int(bases[i] + bases[i+1])
+							i += 1
+						else:
+							jump = int(bases[i])
+						i = i + jump
+						i += 1   # Addition to fix MPileup bug - an additional character is inserted on every addition/deletion 
+					elif base == '*':
+						i += 1
+					elif base == '+':
+						i += 1
+						if bases[i+1].isdigit():    # Addition for insertions longer than 9 bp
+							addNum = int(bases[i] + bases[i+1])
+							i += 1
+						else:
+							addNum = int(bases[i])	
+						addSeq = ''
+						for a in range(addNum):
+							i += 1
+							addSeq += bases[i]
+						i += 1  # Addition to fix MPileup bug - an additional character is inserted on every addition/deletion 
+						types['+'].append(addSeq)
+					else:
+						if types.has_key(base):
+							types[base] += 1
+						else:
+							types['X'].append(base)
+
+					i += 1
+
+				adds = '.'
+				if len(types['+']) > 0:
+						adds = ','.join(types['+'])
+
+				amb = '.'
+				if len(types['X']) > 0:
+						amb = ','.join(types['X'])
+		
+			############################
+			#Write error on file if corrupted data
+			else:
+				chromosome = "CD"
+				bp = "CD"
+				adds = "."
+				amb = "."
+		
+		else:
+			chromosome = "ND"
+			bp = "ND"
+			adds = "."
+			amb = "."
+		############################
+		# End of file analysis
+
+		############################
+		# Print output data - even if file is empty
+
+		out = [FileName1,chromosome,bp,types['A'],types['G'],types['C'],types['T'],types['-'],len(types['+']),adds,amb]
+		print '\t'.join([str(x) for x in out])
+else:
+	############################
+	# Write line of empty output
+	types = {'A':0,'G':0,'C':0,'T':0,'-':0,'+':[],'X':[]}	
+	chromosome = "ND"
+	bp = "ND"
+	adds = "."
+	amb = "."
+	out = [FileName1,chromosome,bp,types['A'],types['G'],types['C'],types['T'],types['-'],len(types['+']),adds,amb]
+	print '\t'.join([str(x) for x in out])
